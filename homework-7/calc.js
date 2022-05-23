d74c64de8063f2985bc788a7501617ab9d33522c
Романчук Александр <alexandr.romanchuk96@gmail.com>
2022-05-23 17:35:34 +0300

    Калькулятор

---
+++ homework-7/calc.js
@@ -0,0 +1,62 @@
+'use scrict';
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const signInput = document.getElementById('sign');
+const buttonСount = document.getElementById('count');
+const buttonReset = document.getElementById('reset');
+const resultOutput = document.getElementById('result');
+const eror = document.querySelector('.eror');
+const sysMsg = document.querySelector('.sys-msg');
+
+buttonСount.addEventListener('click', () => {
+    let value1 = input1.value;
+    let value2 = input2.value;
+    let sign = signInput.value;
+    let result = 0;  
+
+    function resultText(x){
+        resultOutput.value = result;
+        sysMsg.textContent = x;
+    }
+
+    if (value1 == ''){
+        sysMsg.textContent = 'Первое число не указано';
+    }else if (isNaN(value1)){
+        sysMsg.textContent = 'Некорректный ввод чисел';
+    }else if (value2 == ''){
+        sysMsg.textContent = 'Второе число не указано';
+    }else if (isNaN(value2)){
+        sysMsg.textContent = 'Некорректный ввод чисел';
+    }else {
+        if(sign == '') {
+            sysMsg.textContent = 'Необходимо указать операцию +|-|*|/';
+        }else if(sign == '-') {
+            result = +value1 - +value2;
+            resultText('Произведите вычисления');
+        }else if(sign == '/') {
+            if (value2 == '0'){
+                sysMsg.textContent = 'Деление на ноль не определено';
+            }else{
+                result = +value1 / +value2;
+                resultText('Произведите вычисления');
+            }
+        }else if(sign == '*') {
+            result = +value1 * +value2;
+            resultText('Произведите вычисления');
+        }else if (sign == '+'){
+            result = +value1 + +value2;
+            resultText('Произведите вычисления');
+        }else {
+            sysMsg.textContent = 'Программа не поддерживает такую операцию';
+        };
+    };
+});
+
+buttonReset.addEventListener('click', () => {
+    input1.value = '';
+    input2.value = '';
+    signInput.value = '';
+    resultOutput.value = '';
+    sysMsg.textContent = 'Произведите вычисления';
+});
