0e3b7522202405f88d63c7e8185531345e18a33c
Романчук Александр <alexandr.romanchuk96@gmail.com>
2022-06-12 15:31:34 +0300

    Исправлены мелкие баги, добавил дизайн

--- homework-7/calc.js
+++ homework-7/calc.js
@@ -1,11 +1,11 @@
-'use scrict';
+'use strict';
 
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const signInput = document.getElementById('sign');
 const buttonСount = document.getElementById('count');
 const buttonReset = document.getElementById('reset');
-const resultOutput = document.getElementById('result');
+const resultOutput = document.querySelector('.result__display');
 const sysMsg = document.querySelector('.sys-msg');
 
 buttonСount.addEventListener('click', () => {
@@ -15,47 +15,66 @@ buttonСount.addEventListener('click', () => {
     let result;
 
     function resultText(x){
-        resultOutput.value = result;
+        resultOutput.textContent = result;
         sysMsg.textContent = x;
-    }
+    };
 
     if (value1 === ''){
         sysMsg.textContent = 'Первое число не указано';
+        resultOutput.textContent = '0';
     }else if (isNaN(value1)){
         sysMsg.textContent = 'Некорректный ввод чисел';
+        resultOutput.textContent = '0';
     }else if (value2 === ''){
         sysMsg.textContent = 'Второе число не указано';
+        resultOutput.textContent = '0';
     }else if (isNaN(value2)){
         sysMsg.textContent = 'Некорректный ввод чисел';
+        resultOutput.textContent = '0';
     }else {
-        if(sign === '') {
-            sysMsg.textContent = 'Необходимо указать операцию +|-|*|/';
-        }else if(sign === '-') {
-            result = +value1 - +value2;
-            resultText('Произведите вычисления');
-        }else if(sign === '/') {
-            if (value2 === '0'){
-                sysMsg.textContent = 'Деление на ноль не определено';
-            }else{
-                result = +value1 / +value2;
+        switch (sign) {
+            case '': 
+                sysMsg.textContent = 'Необходимо указать операцию +|-|*|/';
+                resultOutput.textContent = '0';
+                break;
+            case '-': 
+                result = +value1 - +value2;
                 resultText('Произведите вычисления');
-            }
-        }else if(sign === '*') {
-            result = +value1 * +value2;
-            resultText('Произведите вычисления');
-        }else if (sign === '+'){
-            result = +value1 + +value2;
-            resultText('Произведите вычисления');
-        }else {
-            sysMsg.textContent = 'Программа не поддерживает такую операцию';
-        };
+                break;
+            case '+': 
+                result = +value1 + +value2;
+                resultText('Произведите вычисления');
+                break;
+            case '*': 
+                result = +value1 * +value2;
+                resultText('Произведите вычисления');
+                break;
+            case '/': 
+                if (value2 === '0' || value2 === '-0') {
+                    sysMsg.textContent = 'Деление на ноль не определено';
+                    resultOutput.textContent = '0';
+                    break;
+                }else{
+                    result = +value1 / +value2;
+                    resultText('Произведите вычисления');
+                    break;
+                }
+            default:
+                sysMsg.textContent = 'Программа не поддерживает такую операцию';
+                resultOutput.textContent = '0';
+                break;
+        }
     };
+});
+
+resultOutput.addEventListener('click', () => {
+    input1.value = resultOutput.textContent;
 });
 
 buttonReset.addEventListener('click', () => {
     input1.value = '';
     input2.value = '';
     signInput.value = '';
-    resultOutput.value = '';
+    resultOutput.textContent = '0';
     sysMsg.textContent = 'Произведите вычисления';
 });
